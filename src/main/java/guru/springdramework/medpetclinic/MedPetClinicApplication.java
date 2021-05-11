b2021da69e2eef16939e1807a1e0d15f8a4aba62
medeniz <medeniz@gmail.com>
2021-05-11 18:16:30 +0200

    Initial project

---
+++ src/main/java/guru/springdramework/medpetclinic/MedPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springdramework.medpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedPetClinicApplication.class, args);
+    }
+
+}
