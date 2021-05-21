dd6de8edaf864fdabd3aa9650624cd48671c6832
medeniz <medeniz@gmail.com>
2021-05-21 12:58:12 +0200

    Added Specialities Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/be/intecbrussel/medpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package be.intecbrussel.medpetclinic.services;
+
+import be.intecbrussel.medpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService <Speciality, Long> {
+}
--- pet-clinic-data/src/main/java/be/intecbrussel/medpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/medpetclinic/services/map/OwnerServiceMap.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
-public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements OwnerService{
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     private final PetTypeService petTypeService;
     private final PetService petService;
@@ -24,34 +24,32 @@ public class OwnerServiceMap extends AbstractMapService<Owner,Long> implements O
     public Set<Owner> findAll() {
         return super.findAll();
     }
+
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
     }
+
     @Override
     public Owner save(Owner object) {
-
-        if (object!=null){
-            if (object.getPets() != null){
+        if (object != null) {
+            if (object.getPets() != null) {
                 object.getPets().forEach(pet -> {
-                    if (pet.getPetType().getId() != null){
+                    if (pet.getPetType().getId() != null) {
                         pet.setPetType(petTypeService.save(pet.getPetType()));
-
-                    }else {
+                    } else {
                         throw new RuntimeException("Pet Type is required");
                     }
-                    if (pet.getId() == null){
+                    if (pet.getId() == null) {
                         Pet savedPet = petService.save(pet);
                         pet.setId(savedPet.getId());
-
                     }
-                } );
+                });
             }
             return super.save(object);
-        }else {
+        } else {
             return null;
         }
-
 
 
     }
