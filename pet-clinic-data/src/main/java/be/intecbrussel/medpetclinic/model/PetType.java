6b86b7d62d9bea23885e7ba7c521203f669c31c6
medeniz <medeniz@gmail.com>
2021-05-28 00:35:48 +0200

    added Project Lombok to project. Closes #51

--- pet-clinic-data/src/main/java/be/intecbrussel/medpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/be/intecbrussel/medpetclinic/model/PetType.java
@@ -1,10 +1,16 @@
 package be.intecbrussel.medpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
@@ -12,11 +18,4 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
